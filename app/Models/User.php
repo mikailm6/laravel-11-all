09bdb19acfa701322e33fb91294d89bbf57296cf
Mikail <mikailyumaanrimalino@gmail.com>
2024-05-03 14:17:48 +0800

    add multi auth feature

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -20,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'type',
     ];
 
     /**
@@ -43,5 +45,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function type() : Attribute
+    {
+        return new Attribute(
+            get: fn($value) => ['user', 'admin', 'manager'][$value],
+        );
     }
 }
---
+++ database/seeders/CreateUsersSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class CreateUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+               'name'=>'Admin User',
+               'email'=>'admin@admin.com',
+               'type'=>1,
+               'password'=> bcrypt('12121212'),
+            ],
+            [
+               'name'=>'Manager User',
+               'email'=>'manager@manager.com',
+               'type'=> 2,
+               'password'=> bcrypt('12121212'),
+            ],
+            [
+               'name'=>'User',
+               'email'=>'user@user.com',
+               'type'=>0,
+               'password'=> bcrypt('12121212'),
+            ],
+        ];
+    
+        foreach ($users as $key => $user) {
+            User::create($user);
+        }
+    }
+}
